7ef901b9cc78ea78720bae4936e4bad186a576ca
Hanas Subakti <hanaskuliah@gmail.com>
2015-12-28 22:25:53 +0800

    update kedua belum seelsai sama sekali

--- app/src/main/java/com/group1/ml/airgestrectrain/UserAddFragment.java
+++ app/src/main/java/com/group1/ml/airgestrectrain/UserAddFragment.java
@@ -5,9 +5,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 
 public class UserAddFragment extends DialogFragment {
@@ -16,5 +18,14 @@ public class UserAddFragment extends DialogFragment {
         View rootView = inflater.inflate(R.layout.fragment_user_add, container, false);
         getDialog().getWindow().setLayout(400,400);
         return rootView;
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Window window = getDialog().getWindow();
+        window.setLayout(400, 400);
+
     }
 }
